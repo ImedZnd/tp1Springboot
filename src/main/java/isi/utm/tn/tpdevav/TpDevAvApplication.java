dddce9855199031b37ea7bfe4f38791a17fdaa5c
imed <imedzenned@yahoo.fr>
2021-02-16 18:39:13 +0100

    first commit

---
+++ src/main/java/isi/utm/tn/tpdevav/TpDevAvApplication.java
@@ -0,0 +1,13 @@
+package isi.utm.tn.tpdevav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpDevAvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpDevAvApplication.class, args);
+    }
+
+}
---
+++ src/test/java/isi/utm/tn/tpdevav/TpDevAvApplicationTests.java
@@ -0,0 +1,13 @@
+package isi.utm.tn.tpdevav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpDevAvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
