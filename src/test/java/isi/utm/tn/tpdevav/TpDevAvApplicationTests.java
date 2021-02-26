dcc669240760c8d7961ec5c6d57304d3a907f1cc
imed <imedzenned@yahoo.fr>
2021-02-26 19:14:40 +0100

    @Disabled disabling the test

--- src/test/java/isi/utm/tn/tpdevav/TpDevAvApplicationTests.java
+++ src/test/java/isi/utm/tn/tpdevav/TpDevAvApplicationTests.java
@@ -1,13 +1,16 @@
 package isi.utm.tn.tpdevav;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class TpDevAvApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
