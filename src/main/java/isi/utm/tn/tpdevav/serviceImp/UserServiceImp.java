7ae5e66acafc845068a8af1132a79a713d8f2d1e
achrafMagh <achraf.magh1995@gmail.com>
2021-03-08 23:19:01 +0100

    bug fixed

--- src/main/java/isi/utm/tn/tpdevav/serviceImp/UserServiceImp.java
+++ src/main/java/isi/utm/tn/tpdevav/serviceImp/UserServiceImp.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
