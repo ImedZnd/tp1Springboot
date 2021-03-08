fad75006a19d09d06b7e5aeb0ae5612ea8237f2f
ahmed <a_belhajamor@yahoo.fr>
2021-03-08 21:54:22 +0100

    role fixed

---
+++ src/main/java/isi/utm/tn/tpdevav/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package isi.utm.tn.tpdevav.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import isi.utm.tn.tpdevav.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> { 
+
+}
