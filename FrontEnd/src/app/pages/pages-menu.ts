7f94a3b0a938d4d42de7b967697615843c0926cd
SI ahmed <a_belhajamor@yahoo.fr>
2021-04-13 00:08:18 +0200

    menu fixed

--- FrontEnd/src/app/pages/pages-menu.ts
+++ FrontEnd/src/app/pages/pages-menu.ts
@@ -2,246 +2,80 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'E-commerce',
-    icon: 'shopping-cart-outline',
+    title: 'News',
+    icon: 'globe-outline',
     link: '/pages/dashboard',
     home: true,
   },
   {
-    title: 'IoT Dashboard',
-    icon: 'home-outline',
+    title: 'Statistics',
+    icon: 'bar-chart-outline',
     link: '/pages/iot-dashboard',
   },
+  // {
+  //   title: 'FEATURES',
+  //   group: true,
+  // },
   {
-    title: 'FEATURES',
-    group: true,
-  },
-  {
-    title: 'Layout',
-    icon: 'layout-outline',
+    title: 'Matches',
+    icon: 'shield-outline',
     children: [
       {
-        title: 'Stepper',
+        title: 'Knockout Phase',
         link: '/pages/layout/stepper',
       },
       {
-        title: 'List',
+        title: 'Group Phase',
         link: '/pages/layout/list',
       },
-      {
-        title: 'Infinite List',
-        link: '/pages/layout/infinite-list',
-      },
-      {
-        title: 'Accordion',
-        link: '/pages/layout/accordion',
-      },
-      {
-        title: 'Tabs',
-        pathMatch: 'prefix',
-        link: '/pages/layout/tabs',
-      },
+
     ],
   },
   {
-    title: 'Forms',
-    icon: 'edit-2-outline',
-    children: [
-      {
-        title: 'Form Inputs',
-        link: '/pages/forms/inputs',
-      },
-      {
-        title: 'Form Layouts',
-        link: '/pages/forms/layouts',
-      },
-      {
-        title: 'Buttons',
-        link: '/pages/forms/buttons',
-      },
-      {
-        title: 'Datepicker',
-        link: '/pages/forms/datepicker',
-      },
-    ],
+    title: 'Teams',
+    icon: 'people-outline',
   },
   {
-    title: 'UI Features',
-    icon: 'keypad-outline',
+    title: 'Groups',
+    icon: 'grid-outline',
     link: '/pages/ui-features',
     children: [
       {
-        title: 'Grid',
+        title: 'Group A',
         link: '/pages/ui-features/grid',
       },
       {
-        title: 'Icons',
+        title: 'Group B',
         link: '/pages/ui-features/icons',
       },
       {
-        title: 'Typography',
+        title: 'Group C',
         link: '/pages/ui-features/typography',
       },
       {
-        title: 'Animated Searches',
+        title: 'Group D',
+        link: '/pages/ui-features/search-fields',
+      },
+      {
+        title: 'Group E',
+        link: '/pages/ui-features/search-fields',
+      },
+      {
+        title: 'Group F',
+        link: '/pages/ui-features/search-fields',
+      },
+      {
+        title: 'Group G',
+        link: '/pages/ui-features/search-fields',
+      },
+      {
+        title: 'Group H',
         link: '/pages/ui-features/search-fields',
       },
     ],
   },
   {
-    title: 'Modal & Overlays',
-    icon: 'browser-outline',
-    children: [
-      {
-        title: 'Dialog',
-        link: '/pages/modal-overlays/dialog',
-      },
-      {
-        title: 'Window',
-        link: '/pages/modal-overlays/window',
-      },
-      {
-        title: 'Popover',
-        link: '/pages/modal-overlays/popover',
-      },
-      {
-        title: 'Toastr',
-        link: '/pages/modal-overlays/toastr',
-      },
-      {
-        title: 'Tooltip',
-        link: '/pages/modal-overlays/tooltip',
-      },
-    ],
-  },
-  {
-    title: 'Extra Components',
-    icon: 'message-circle-outline',
-    children: [
-      {
-        title: 'Calendar',
-        link: '/pages/extra-components/calendar',
-      },
-      {
-        title: 'Progress Bar',
-        link: '/pages/extra-components/progress-bar',
-      },
-      {
-        title: 'Spinner',
-        link: '/pages/extra-components/spinner',
-      },
-      {
-        title: 'Alert',
-        link: '/pages/extra-components/alert',
-      },
-      {
-        title: 'Calendar Kit',
-        link: '/pages/extra-components/calendar-kit',
-      },
-      {
-        title: 'Chat',
-        link: '/pages/extra-components/chat',
-      },
-    ],
-  },
-  {
-    title: 'Maps',
-    icon: 'map-outline',
-    children: [
-      {
-        title: 'Google Maps',
-        link: '/pages/maps/gmaps',
-      },
-      {
-        title: 'Leaflet Maps',
-        link: '/pages/maps/leaflet',
-      },
-      {
-        title: 'Bubble Maps',
-        link: '/pages/maps/bubble',
-      },
-      {
-        title: 'Search Maps',
-        link: '/pages/maps/searchmap',
-      },
-    ],
-  },
-  {
-    title: 'Charts',
-    icon: 'pie-chart-outline',
-    children: [
-      {
-        title: 'Echarts',
-        link: '/pages/charts/echarts',
-      },
-      {
-        title: 'Charts.js',
-        link: '/pages/charts/chartjs',
-      },
-      {
-        title: 'D3',
-        link: '/pages/charts/d3',
-      },
-    ],
-  },
-  {
-    title: 'Editors',
-    icon: 'text-outline',
-    children: [
-      {
-        title: 'TinyMCE',
-        link: '/pages/editors/tinymce',
-      },
-      {
-        title: 'CKEditor',
-        link: '/pages/editors/ckeditor',
-      },
-    ],
-  },
-  {
-    title: 'Tables & Data',
-    icon: 'grid-outline',
-    children: [
-      {
-        title: 'Smart Table',
-        link: '/pages/tables/smart-table',
-      },
-      {
-        title: 'Tree Grid',
-        link: '/pages/tables/tree-grid',
-      },
-    ],
-  },
-  {
-    title: 'Miscellaneous',
-    icon: 'shuffle-2-outline',
-    children: [
-      {
-        title: '404',
-        link: '/pages/miscellaneous/404',
-      },
-    ],
-  },
-  {
-    title: 'Auth',
-    icon: 'lock-outline',
-    children: [
-      {
-        title: 'Login',
-        link: '/auth/login',
-      },
-      {
-        title: 'Register',
-        link: '/auth/register',
-      },
-      {
-        title: 'Request Password',
-        link: '/auth/request-password',
-      },
-      {
-        title: 'Reset Password',
-        link: '/auth/reset-password',
-      },
-    ],
+    title: 'AWARDS',
+    icon: 'star-outline',
   },
 ];
